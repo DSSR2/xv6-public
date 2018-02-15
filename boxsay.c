a288cda909b5622f5c5f550b4375013d4f0cdafa
DSSR2 <dineshsathiraj2@gmail.com>
2018-02-15 19:43:48 +0530

    Fixed boxsay

--- boxsay.c
+++ boxsay.c
@@ -5,13 +5,17 @@ int
 main(int argc, char *argv[]){
   int i;
   int b;
-  b=strlen(argv[1]);
+  if(argc==1)
+    exit();
+  b=0;
+  for(i=1;i<argc;i++)
+    b+=strlen(argv[i])+1;
   for(i = 1; i < b+8; i++)
     printf(1, "_");
   printf(1,"\n||  ");
   for(i=1; i< argc; i++)
-	printf(1, "%s%s", argv[1], i+1 < argc ? " " : " ");
-  printf(1,"  ||\n");
+	printf(1, "%s%s", argv[i], i+1 < argc ? " " : " ");
+  printf(1," ||\n");
   for(i = 1; i < b+8; i++)
     printf(1, "`");
   printf(1,"\n");
--- one.c
+++ one.c
@@ -5,6 +5,11 @@
 
 int
 main(int argc, char *argv[]){
+  if(argv[1][0])
+  printf(1,"%d\n", argc);
+  exit();
+  /*
   printf(1,"%c, %d\n", argv[1][0], argv[1][0]);
   exit();
+  */
 }
